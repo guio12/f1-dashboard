5ce299fab6f8588e51f7b85ce32eb0fb0d84ac1a
guio12 <guillaume.audouze@hakisa.com>
2024-03-13 18:02:25 +0100

    changement d'extension fichiers config prettier + stylelint

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-  plugins: ['prettier-plugin-tailwindcss'],
-  tabWidth: 2,
-  trailingComma: 'es5',
-  semi: false,
-  singleQuote: true,
-  vueIndentScriptAndStyle: true,
-}
--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: [
-    'stylelint-config-idiomatic-order',
-    'stylelint-config-recommended-vue',
-  ],
-  rules: {},
-}
