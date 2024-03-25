8c822fef510f76eb2433f94121f299b3e4e11dd9
guio12 <guillaume.audouze@hakisa.com>
2024-03-25 16:43:40 +0100

    menu : retour du UVerticalNavigation

--- components/menu/MenuDisplay.vue
+++ components/menu/MenuDisplay.vue
@@ -1,52 +1,28 @@
 <script setup lang="ts">
-  const menuItems = [
-    {
-      label: 'Prochain Grand Prix',
-      icon: 'i-heroicons-arrow-right-circle-solid',
-      //to: '/',
-    },
-    {
-      label: 'Saisons',
-      icon: 'i-heroicons-calendar-days-solid',
-      //to: '/season-2023/grand-prix',
-    },
-  ]
-
-  const items = [
-    {
-      label: 'Saisons',
-      icon: 'i-heroicons-information-circle',
-      defaultOpen: true,
-      slot: 'seasons',
-    },
-  ]
+const menuItems = [
+  {
+    label: 'Prochain Grand Prix',
+    icon: 'i-heroicons-arrow-right-circle-solid',
+    to: '/',
+  },
+  {
+    label: 'Saison 2024',
+    icon: 'i-heroicons-calendar-days-solid',
+    to: '/season/2024',
+  },
+  {
+    label: 'Saisons précédentes',
+    icon: 'i-heroicons-archive-box-arrow-down-16-solid',
+    to: '/past-seasons/',
+  },
+]
 </script>
 
 <template>
   <aside
     class="sidebar invisible fixed h-full w-auto bg-gray-800 p-7 lg:visible"
   >
-    <div>
-      <UButton
-        to="/"
-        label="Prochain Grand Prix"
-        icon="i-heroicons-arrow-right-circle-solid"
-      />
-    </div>
-
-    <div class="mt-5">
-      <UAccordion :items="items">
-        <template #seasons>
-          <div class="text-center">
-            <ULink to="/season/2024">2024</ULink>
-          </div>
-
-          <div class="text-center">
-            <ULink to="/season/2023">2023</ULink>
-          </div>
-        </template>
-      </UAccordion>
-    </div>
+    <UVerticalNavigation :links="menuItems" />
   </aside>
 
   <header class="bg-gray-800 lg:invisible">
