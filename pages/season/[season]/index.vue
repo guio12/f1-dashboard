826a1aa96a046e9b8c5ff9845a6ca69535800285
guio12 <guillaume.audouze@hakisa.com>
2024-03-25 16:41:17 +0100

    intégration du DatePicker dans la liste des GP

--- pages/season/[season]/index.vue
+++ pages/season/[season]/index.vue
@@ -1,20 +1,90 @@
 <script setup lang="ts">
-  const { season } = useRoute().params
+const { season } = useRoute().params
 
-  useHead({
-    title: `Saison ${season}`,
-  })
+useHead({
+  title: `Saison ${season}`,
+})
 
-  const { data: seasonData } = await useFetch<GrandPrixOverviewData[]>(
-    `/api/season/${season}`
+const { data: seasonData } = await useFetch<GrandPrixOverviewData[]>(
+  `/api/season/${season}`
+)
+
+// Date range picker
+import { format } from 'date-fns'
+
+/* const seasonStartDate = ref<Date>()
+  const seasonEndDate = ref<Date>() */
+const seasonStartDate = ref()
+const seasonEndDate = ref()
+
+if (seasonData.value) {
+  seasonStartDate.value = new Date(seasonData.value[0].dateStart)
+  seasonEndDate.value = new Date(
+    seasonData.value[seasonData.value.length - 1].dateEnd
   )
+}
+
+const selectedDates = ref({
+  start: seasonStartDate,
+  end: seasonEndDate,
+})
+
+const selectAllSeasonRange = () => {
+  selectedDates.value.start = seasonStartDate
+  selectedDates.value.end = seasonEndDate
+}
+
+const filteredSeasonData = computed(() => {
+  return seasonData.value?.filter((data) => {
+    const selectedDateStart = new Date(selectedDates.value.start)
+    const selectedDateEnd = new Date(selectedDates.value.end)
+    const gpDateStart = new Date(data.dateStart)
+    const gpDateEnd = new Date(data.dateEnd)
+
+    if (selectedDateStart <= gpDateStart && selectedDateEnd >= gpDateEnd) {
+      return data
+    }
+  })
+})
+
+const numberOfGPs = computed(() => {
+  return filteredSeasonData.value ? filteredSeasonData.value.length : 0
+})
 </script>
 
 <template>
   <article class="mt-5 grid grid-cols-2 gap-10">
     <h1>Saison {{ season }}</h1>
 
-    <section v-for="grandPrix in seasonData" :key="grandPrix.round">
+    <UPopover :popper="{ placement: 'bottom-start' }">
+      <UButton icon="i-heroicons-calendar-days-20-solid">
+        {{ format(selectedDates.start, 'd MMM, yyy') }} -
+        {{ format(selectedDates.end, 'd MMM, yyy') }}
+      </UButton>
+
+      <template #panel="{ close }">
+        <div
+          class="flex items-center divide-gray-200 sm:divide-x dark:divide-gray-800"
+        >
+          <div class="hidden flex-col py-4 sm:flex">
+            <UButton
+              label="Saison entière"
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-calendar-days-20-solid"
+              class="rounded-none px-6"
+              truncate
+              @click="selectAllSeasonRange()"
+            />
+          </div>
+          <UIDatePicker v-model="selectedDates" @close="close" />
+        </div>
+      </template>
+    </UPopover>
+
+    <p>Nombre de Grand Prix durant cette période : {{ numberOfGPs }}</p>
+
+    <section v-for="grandPrix in filteredSeasonData" :key="grandPrix.round">
       <GrandPrixOverview
         :season="grandPrix.season"
         :round="grandPrix.round"
