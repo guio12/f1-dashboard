b442c8c46196369bc461fa2e18f73e5fccfaac9a
guio12 <guillaume.audouze@hakisa.com>
2024-03-25 16:42:12 +0100

    changement de l'organisation des pages : nouvelle page des saisons précédentes

---
+++ pages/past-seasons/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: 'Saisons précédentes',
+})
+</script>
+
+<template>
+  <article class="">
+    <h1>Saisons précédentes</h1>
+
+    <section>
+      <NuxtLink to="/season/2023">Saison 2023</NuxtLink>
+      <NuxtLink to="/season/2022">Saison 2022</NuxtLink>
+    </section>
+  </article>
+</template>
