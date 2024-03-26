8a6c6397c9b0ab80205bde6d823ba21be343b677
guio12 <guillaume.audouze@hakisa.com>
2024-03-26 16:38:03 +0100

    WIP réarragenement de la Card d'un GP (overview)

--- components/GrandPrixOverview.vue
+++ components/GrandPrixOverview.vue
@@ -16,29 +16,33 @@ const formatTime = (date: string, time: string) =>
 </script>
 
 <template>
-  <UCard>
-    <template #header>
-      <div>
-        <ULink :to="url" class="link">{{ name }}</ULink>
-        <p class="h-8">Round {{ round }}</p>
-        <UBadge v-if="isSprintGrandPrix" label="Sprint" />
-      </div>
-    </template>
+  <section>
+    <UCard>
+      <template #header>
+        <div>
+          <ULink :to="url" class="link">{{ name }}</ULink>
 
-    <div class="h-8">
-      {{ formatDate(dateStart) }} - {{ formatDate(dateEnd) }}
-    </div>
-
-    <div class="h-8">Début de la course à {{ formatTime(dateEnd, time) }}</div>
-
-    <template #footer>
+          <div class="flex items-start justify-between">
+            <p class="h-8">Round {{ round }}</p>
+            <UBadge v-if="isSprintGrandPrix" label="Sprint" />
+          </div>
+        </div>
+      </template>
       <div class="h-8">
-        <UButton
-          icon="i-heroicons-information-circle-solid"
-          :to="`/season/${season}/grand-prix/${round}`"
-          >Détails du Grand Prix</UButton
-        >
+        {{ formatDate(dateStart) }} - {{ formatDate(dateEnd) }}
       </div>
-    </template>
-  </UCard>
+      <div class="h-8">
+        Départ de la course à {{ formatTime(dateEnd, time) }}
+      </div>
+      <template #footer>
+        <div class="h-8">
+          <UButton
+            icon="i-heroicons-information-circle-solid"
+            :to="`/season/${season}/grand-prix/${round}`"
+            >Détails du Grand Prix</UButton
+          >
+        </div>
+      </template>
+    </UCard>
+  </section>
 </template>
