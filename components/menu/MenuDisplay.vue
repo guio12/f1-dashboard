e15362927cba21a9edd7dea0103ede6f6398ae93
guio12 <guillaume.audouze@hakisa.com>
2024-03-29 15:23:58 +0100

    WIP color mode & menu

--- components/menu/MenuDisplay.vue
+++ components/menu/MenuDisplay.vue
@@ -18,16 +18,51 @@ const menuItems = [
 ]
 
 const isMobileMenuOpen = ref(false)
+
+const colorMode = useColorMode()
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  },
+})
 </script>
 
 <template>
-  <header class="bg-gray-800 px-10 py-3">
-    <UHorizontalNavigation :links="menuItems" class="hidden lg:block" />
+  <header
+    class="flex justify-between border-b border-gray-200 px-5 lg:justify-normal lg:px-0 dark:border-gray-800"
+  >
+    <UHorizontalNavigation :links="menuItems" class="hidden ps-10 lg:block">
+      <template #default="{ link }">
+        <span class="relative py-2">{{ link.label }}</span>
+      </template>
+
+      <template #icon="{ link }">
+        <span :class="link.icon" class="group-hover:text-primary"></span>
+      </template>
+    </UHorizontalNavigation>
     <UButton
       icon="i-heroicons-bars-3-16-solid"
-      class="lg:hidden"
+      class="my-5 me-10 lg:my-0 lg:hidden"
       @click="isMobileMenuOpen = true"
     />
+    <ClientOnly>
+      <UButton
+        :icon="
+          isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+        "
+        color="gray"
+        variant="ghost"
+        aria-label="Theme"
+        class="rounded-none border-gray-200 lg:me-10 lg:border-b dark:border-gray-800"
+        @click="isDark = !isDark"
+      />
+      <template #fallback>
+        <div class="h-8 w-8" />
+      </template>
+    </ClientOnly>
     <MenuMobile v-model="isMobileMenuOpen" :menu-items="menuItems" />
   </header>
 </template>
