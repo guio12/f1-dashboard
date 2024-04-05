896f56eb5295e421829a24edde30a227e9b1d981
guio12 <guillaume.audouze@hakisa.com>
2024-04-05 17:27:23 +0200

    quelques changements dans l'affichage de la liste des GPs

--- components/GrandPrixOverview.vue
+++ components/GrandPrixOverview.vue
@@ -3,6 +3,7 @@ defineProps<{
   season: string
   round: string
   name: string
+  circuitName: string
   dateStart: string
   dateEnd: string
   time: string
@@ -13,27 +14,46 @@ defineProps<{
 const formatDate = (date: string) => dateFormatter(date)
 const formatTime = (date: string, time: string) =>
   timeFormatter(`${date}T${time}`)
+
+const formatCircuitName = (name: string) => `/img/${slugify(name)}.webp`
 </script>
 
 <template>
   <section>
-    <UCard>
+    <UCard :ui="{ background: 'bg-gray-50 dark:bg-gray-900' }">
       <template #header>
         <div>
-          <ULink :to="url" class="link">{{ name }}</ULink>
-
           <div class="flex items-start justify-between">
-            <p class="h-8">Round {{ round }}</p>
+            <ULink :to="url" target="_blank" class="link flex items-center"
+              >{{ name }}
+              <UIcon
+                name="i-heroicons-arrow-top-right-on-square-16-solid"
+                class="ms-1"
+            /></ULink>
             <UBadge v-if="isSprintGrandPrix" label="Sprint" />
           </div>
+
+          <div class="flex justify-between items-center h-8">
+            <span>Round {{ round }}</span>
+            <Icon name="i-heroicons-arrow-right-solid" />
+            <span class="font-bold">
+              {{ formatDate(dateStart) }} -
+              {{ formatDate(dateEnd) }}
+            </span>
+          </div>
+        </div>
+        <div class="h-6">
+          <span>Départ de la course à</span> <span class="font-bold">{{ formatTime(dateEnd, time) }}</span>
         </div>
       </template>
-      <div class="h-8">
-        {{ formatDate(dateStart) }} - {{ formatDate(dateEnd) }}
-      </div>
-      <div class="h-8">
-        Départ de la course à {{ formatTime(dateEnd, time) }}
-      </div>
+      <figure>
+        <img
+          :src="formatCircuitName(circuitName)"
+          :alt="circuitName"
+          :title="circuitName"
+        />
+        <figcaption class="text-center text-sm italic">© FIA</figcaption>
+      </figure>
       <template #footer>
         <div class="h-8">
           <UButton
