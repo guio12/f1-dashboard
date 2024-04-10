ce53091f5535b3a181dd2acf43be10538355d78b
guio12 <guillaume.audouze@hakisa.com>
2024-04-10 11:15:18 +0200

    ajout d'un composant pour l'affichage de la carte d'un circuit

---
+++ components/circuit/CircuitMap.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  circuitName: string
+}>()
+
+const formatCircuitName = (name: string) => `/img/${slugify(name)}.webp`
+</script>
+
+<template>
+  <figure>
+    <img
+      :src="formatCircuitName(circuitName)"
+      :alt="circuitName"
+      :title="circuitName"
+      class="m-auto p-5"
+    />
+    <figcaption class="text-center text-sm italic">© FIA</figcaption>
+  </figure>
+</template>
