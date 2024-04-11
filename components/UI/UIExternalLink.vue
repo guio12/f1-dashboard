165a27beb486f03b6f36f2f1ff27b579674fc93d
guio12 <guillaume.audouze@hakisa.com>
2024-04-11 10:39:55 +0200

    nouveau composant UI : lien externe

---
+++ components/UI/UIExternalLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  url: string
+  name: string
+}>()
+</script>
+
+<template>
+  <ULink :to="url" target="_blank" class="link inline-flex items-center">
+    {{ name }}
+    <UIcon name="i-heroicons-arrow-top-right-on-square-16-solid" class="ms-1" />
+  </ULink>
+</template>
