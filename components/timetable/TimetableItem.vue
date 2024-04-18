184239235af24b905d6ef6d69929f7d33221b333
guio12 <guillaume.audouze@hakisa.com>
2024-04-18 11:31:57 +0200

    WIP nouveau composant TimetableItem

---
+++ components/timetable/TimetableItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  session: TimelineItemData
+}>()
+
+const splitDate = computed(() => {
+  return props.session.date.split(' ')
+})
+</script>
+
+<template>
+  <div
+    class="group mx-auto mt-1 flex items-center justify-between rounded bg-gray-400 p-4 md:w-3/4"
+  >
+    <div class="text-center text-xl">
+      <div class="font-bold">{{ splitDate[0] }}</div>
+      <div>{{ splitDate[1] }}</div>
+    </div>
+    <div class="text-xl">{{ session.sessionName }}</div>
+    <div class="text-xl font-bold">{{ session.heure }}</div>
+  </div>
+</template>
---
+++ types/TimelineItemData.ts
@@ -0,0 +1,5 @@
+export type TimelineItemData = {
+  sessionName: string
+  date: string
+  heure: string
+}
