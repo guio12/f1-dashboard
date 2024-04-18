abdfd184118a66ba1f8c13b274ccf930d333cc17
guio12 <guillaume.audouze@hakisa.com>
2024-04-18 11:37:26 +0200

    nouveau composant MenuSeason pour la gestion des routes par rapport aux saisons (sous-menu)

---
+++ components/menu/MenuSeason.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const links = [
+  {
+    label: '2022',
+    to: '/season/2022',
+  },
+  {
+    label: '2023',
+    to: '/season/2023',
+  },
+  {
+    label: '2024',
+    to: '/season/2024',
+  },
+]
+</script>
+
+<template>
+  <UHorizontalNavigation
+    :links="links"
+    class="max-w-fit border-b border-gray-200 dark:border-gray-800"
+  />
+</template>
