3b1c1780ffda9b78a8394af70db66b2a7b2fe6e0
guio12 <guillaume.audouze@hakisa.com>
2024-04-18 11:36:15 +0200

    WIP amélioration des widgets de la liste des GPs

--- components/GrandPrixOverview.vue
+++ components/GrandPrixOverview.vue
@@ -4,6 +4,7 @@ defineProps<{
   round: string
   name: string
   circuitName: string
+  countryName: string
   dateStart: string
   dateEnd: string
   time: string
@@ -14,55 +15,47 @@ defineProps<{
 const formatDate = (date: string) => dateFormatter(date)
 const formatTime = (date: string, time: string) =>
   timeFormatter(`${date}T${time}`)
-
-const formatCircuitName = (name: string) => `/img/${slugify(name)}.webp`
 </script>
 
 <template>
   <section>
-    <UCard :ui="{ background: 'bg-gray-50 dark:bg-gray-900' }">
-      <template #header>
-        <div>
-          <div class="flex items-start justify-between">
-            <ULink :to="url" target="_blank" class="link flex items-center"
-              >{{ name }}
-              <UIcon
-                name="i-heroicons-arrow-top-right-on-square-16-solid"
-                class="ms-1"
-            /></ULink>
-            <UBadge v-if="isSprintGrandPrix" label="Sprint" />
+    <ULink :to="`/season/${season}/grand-prix/${round}`">
+      <UCard
+        :ui="{ background: 'bg-gray-50 dark:bg-gray-900' }"
+        class="border border-transparent hover:border-green-400"
+      >
+        <template #header>
+          <div>
+            <div class="flex items-start justify-between">
+              <div>
+                <UIcon
+                  :name="matchCountryFlag(countryName)"
+                  class="me-2 align-text-bottom text-2xl"
+                />
+                <span class="text-xl">{{ name }}</span>
+              </div>
+              <UBadge v-if="isSprintGrandPrix" label="Sprint" />
+            </div>
+            <div class="flex h-8 items-center justify-between">
+              <span>Round {{ round }}</span>
+              <UDivider
+                orientation="vertical"
+                size="lg"
+                class="mx-5 text-red-600"
+              />
+              <span class="font-bold">
+                {{ formatDate(dateStart) }} -
+                {{ formatDate(dateEnd) }}
+              </span>
+            </div>
           </div>
-
-          <div class="flex justify-between items-center h-8">
-            <span>Round {{ round }}</span>
-            <Icon name="i-heroicons-arrow-right-solid" />
-            <span class="font-bold">
-              {{ formatDate(dateStart) }} -
-              {{ formatDate(dateEnd) }}
-            </span>
+          <div class="h-6">
+            <span>Départ de la course à </span>
+            <span class="font-bold">{{ formatTime(dateEnd, time) }}</span>
           </div>
-        </div>
-        <div class="h-6">
-          <span>Départ de la course à</span> <span class="font-bold">{{ formatTime(dateEnd, time) }}</span>
-        </div>
-      </template>
-      <figure>
-        <img
-          :src="formatCircuitName(circuitName)"
-          :alt="circuitName"
-          :title="circuitName"
-        />
-        <figcaption class="text-center text-sm italic">© FIA</figcaption>
-      </figure>
-      <template #footer>
-        <div class="h-8">
-          <UButton
-            icon="i-heroicons-information-circle-solid"
-            :to="`/season/${season}/grand-prix/${round}`"
-            >Détails du Grand Prix</UButton
-          >
-        </div>
-      </template>
-    </UCard>
+        </template>
+        <CircuitMap :circuit-name="circuitName" />
+      </UCard>
+    </ULink>
   </section>
 </template>
