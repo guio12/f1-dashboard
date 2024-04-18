9cd8e7611a10b84cc1b9353aec982ef3e6de40b0
guio12 <guillaume.audouze@hakisa.com>
2024-04-18 11:40:04 +0200

    ajout du drapeau dans la page de détail d'un GP

--- pages/season/[season]/grand-prix/[round].vue
+++ pages/season/[season]/grand-prix/[round].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const { season, round } = useRoute().params
+
 const { data: grandPrixData } = await useFetch<GrandPrixDetailsData>(
   `/api/season/${season}/grand-prix/${round}`
 )
@@ -10,13 +11,14 @@ useHead({
 </script>
 
 <template>
-  <article v-if="grandPrixData">
-    <h1>
-      <NuxtLink :to="grandPrixData?.url">{{ grandPrixData?.name }}</NuxtLink>
-    </h1>
-    <h2>Saison {{ season }} | Round {{ round }}</h2>
-
-    <section>
+  <article v-if="grandPrixData" class="page-container">
+    <section class="mx-auto lg:max-w-4xl">
+      <div class="flex items-center">
+        <UIcon :name="matchCountryFlag(grandPrixData.circuit.country)" class="text-4xl me-4" />
+        <h1 class="self-center">
+          <UIExternalLink :name="grandPrixData.name" :url="grandPrixData.url" />
+        </h1>
+      </div>
       <GrandPrixDetails :grand-prix-data="grandPrixData" />
     </section>
   </article>
