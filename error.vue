d6f68e83bb517645ac6e609618562b6f0086fca4
guio12 <guillaume.audouze@hakisa.com>
2024-06-19 10:31:34 +0200

    page d'erreur custom

---
+++ error.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+useHead({
+  bodyAttrs: {
+    class: 'bg-white dark:bg-gray-900',
+  },
+})
+
+defineProps({
+  error: {
+    type: Object as () => NuxtError,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="text-center mt-36">
+      <h1>{{ error.statusCode }}</h1>
+
+      <pre>{{ error }}</pre>
+    </div>
+  </NuxtLayout>
+</template>
