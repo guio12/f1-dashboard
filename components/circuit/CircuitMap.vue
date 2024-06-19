a052a1fa3bdb5e8dc1b150dc68cf1c6c8341df61
guio12 <guillaume.audouze@hakisa.com>
2024-06-19 11:13:55 +0200

    CircuitMap.vue : gestion des erreurs

--- components/circuit/CircuitMap.vue
+++ components/circuit/CircuitMap.vue
@@ -4,16 +4,22 @@ defineProps<{
 }>()
 
 const formatCircuitName = (name: string) => `/img/${slugify(name)}.webp`
+const imageError = ref(false)
+const handleError = () => {
+  imageError.value = true
+}
 </script>
 
 <template>
-  <figure>
+  <figure v-if="!imageError">
     <img
       :src="formatCircuitName(circuitName)"
-      :alt="circuitName"
+      :alt="`Carte du circuit de ${circuitName}`"
       :title="circuitName"
       class="m-auto"
+      @error="handleError"
     />
     <figcaption class="text-center text-sm italic">© FIA</figcaption>
   </figure>
+  <div v-else class="m-auto h-10 text-center">Image introuvable</div>
 </template>
